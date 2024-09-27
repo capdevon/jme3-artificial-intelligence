6e5764a4b4c98dda02fa26111969b4f7d3e70a56
capdevon <jefferydeaver2010@gmail.com>
2024-09-27 16:49:27 +0200

    add NavMeshUserData class

---
+++ jme3-ai-examples/src/main/java/com/jme3/ai/navmesh/gen/NavMeshUserData.java
@@ -0,0 +1,11 @@
+package com.jme3.ai.navmesh.gen;
+
+/**
+ * 
+ * @author capdevon
+ */
+public interface NavMeshUserData {
+    
+    public final String JME_NAVMESH_IGNORE = "JmeNavMeshIgnore";
+
+}
